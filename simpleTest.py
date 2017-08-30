17ba2fa92cb82463d459b53dd46a1a001ecd0b0e
osmaralg <osmar.alcala94@gmail.com>
2017-08-30 17:40:27 -0500

    obtener coordenada x y y del los 400 rayos

--- simpleTest.py
+++ simpleTest.py
@@ -24,6 +24,21 @@
 #
 # IMPORTANT: for each successful call to simxStart, there
 # should be a corresponding call to simxFinish at the end!
+import math
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+
+def draw_lrf(lrf):
+    lrf = np.asarray(lrf)
+    fig = plt.figure()
+    ax = fig.add_subplot(111, aspect='equal')
+    ax.plot(0, 0, 'r>', markersize=10)
+    ax.scatter(lrf[:, 0], lrf[:, 1])
+    ax.set_xlim([-3, 3])
+    ax.set_ylim([-3, 3])
+    ax.grid()
+    plt.show()
 
 try:
     import vrep
@@ -36,13 +51,12 @@ except:
     print ('--------------------------------------------------------------')
     print ('')
 
-import time
+
 
 print ('Program started')
 vrep.simxFinish(-1) # just in case, close all opened connections
 clientID=vrep.simxStart('127.0.0.1',19997,True,True,5000,5) # Connect to V-REP
 if clientID!=-1:
-    print ('Connected to remote API server')
     print ('Connected to remote API server')
     # Now try to retrieve data in a blocking fashion (i.e. a service call):
     res,objs=vrep.simxGetObjects(clientID,vrep.sim_handle_all,vrep.simx_opmode_blocking)
@@ -50,22 +64,18 @@ if clientID!=-1:
     returnCode,back_right=vrep.simxGetObjectHandle(clientID,'back_right_motor',vrep.simx_opmode_blocking)
     returnCode,front_left=vrep.simxGetObjectHandle(clientID,'front_left_motor',vrep.simx_opmode_blocking)
     returnCode,front_right=vrep.simxGetObjectHandle(clientID,'front_right_motor',vrep.simx_opmode_blocking)
-    returnCode,Sensor_motor=vrep.simxGetObjectHandle(clientID,'Sensor_motor',vrep.simx_opmode_blocking)
-    print ('Sensor_motor handle is:',Sensor_motor)
+  
+
+    if returnCode==vrep.simx_return_ok:
+        print ('Corrio bien la lectura del handle')
+    else:
+        print ('Remote API function call returned with error code: ',returnCode)
+    print ('No sirvio el returnCode')    
     print ('left_motor handle is:',back_left)
     print ('left_motor handle is:',front_left)
     print ('right_motor handle is:',back_right)
     print ('right_motor handle is:',front_right)
-	#print ('Sensor_motor handle is:',Sensor_motor)
-	#print ('Back left handle is:')
-	#print('Back right handle is:')
-	#print('Front left handle is:')
-	#print('Front right handle is:')
-    returnCode=vrep.simxSetJointTargetVelocity(clientID,Sensor_motor,1,vrep.simx_opmode_streaming)
-	#print(back_left)
-	#print("Back righthandle is:",back_right)
-	#print("Front lefthandle is:",front_left)
-	#print("Front righthandle is:",front_right)
+	
     if res==vrep.simx_return_ok:
         print ('Number of objects in the scene: ',len(objs))
     else:
@@ -75,12 +85,30 @@ if clientID!=-1:
 
     # Now retrieve streaming data (i.e. in a non-blocking fashion):
     startTime=time.time()
+
+    name_hokuyo_data = "hokuyo_data"
+    vrep.simxGetStringSignal(clientID, name_hokuyo_data,
+                             vrep.simx_opmode_streaming)
     vrep.simxGetIntegerParameter(clientID,vrep.sim_intparam_mouse_x,vrep.simx_opmode_streaming) # Initialize streaming
-    while time.time()-startTime < .01:
+    while time.time()-startTime < 10:
         returnCode,data=vrep.simxGetIntegerParameter(clientID,vrep.sim_intparam_mouse_x,vrep.simx_opmode_buffer) # Try to retrieve the streamed data
+        e, lrf_bin = vrep.simxGetStringSignal(clientID, name_hokuyo_data,
+                                              vrep.simx_opmode_buffer)
+        lrf_raw = vrep.simxUnpackFloats(lrf_bin)
+        lrf = np.array(lrf_raw).reshape(-1, 3)
+    for i in range (0,399):        
+        magnitud=(lrf[i,0]*lrf[i,0]+lrf[i,1]*lrf[i,1])
+        print ('la magnitud es:',magnitud)
+        #print ('hola sensor',lrf)
+        
         if returnCode==vrep.simx_return_ok: # After initialization of streaming, it will take a few ms before the first value arrives, so check the return code
             print ('Mouse position x: ',data) # Mouse position x is actualized when the cursor is over V-REP's window
-        time.sleep(0.005)
+            draw_lrf(lrf)
+			#returnCode,Sensor_motor_pos=vrep.simxGetJointPosition(clientID,Sensor_motor,vrep.simx_opmode_blocking)
+			#print('La posicion del motor es:',Sensor_motor_pos)
+			
+				
+        time.sleep(0.05)
 
     # Now send some data to V-REP in a non-blocking fashion:
     vrep.simxAddStatusbarMessage(clientID,'Hello V-REP!',vrep.simx_opmode_oneshot)
